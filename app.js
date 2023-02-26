f9aaefa30f6023d950f7adc5362d25f1742da33d
peggy8865 <peggy8865@gmail.com>
2023-02-26 18:00:37 +0800

    add express-session and add login and register feature

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+const passport = require('passport')
+const usePassport = require('./config/passport')
 const app = express()
 const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecondSecretYo',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -18,32 +29,43 @@ app.get('/', (req, res) => {
     nest: true
   })
     .then(todos => res.render('index', { todos: todos }))
-    .catch(error => res.status(422).json(error))
+    .catch(err => res.status(422).json(err))
 })
 
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
+    .catch(err => console.log(err))
 })
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login!')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
 
 app.post('/users/register', (req, res) => {
-  const { name, email, password } = req.body
-  User.create({ name, email, password })
-    .then(() => res.redirect('/'))
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('user already exists')
+      return res.render('register', { name, email, password, confirmPassword })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({ name, email, password: hash })
+        .then(() => res.redirect('/')))
+      .catch(err => console.log(err))
+  })
 })
 
 app.get('/users/logout', (req, res) => {
